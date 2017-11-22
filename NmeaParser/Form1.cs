3a4230477770c0b778242684e3b193aa836e484d
ReneMat <rene.mat@email.cz>
2017-11-22 12:27:30 +0100

    gga

--- NmeaParser/Form1.cs
+++ NmeaParser/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NmeaParser.Business;
+
 
 namespace NmeaParser
 {
     public partial class Form1 : Form
     {
+        private NMEA nmeaParser;
+             
         public Form1()
         {
             InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            nmeaParser = new NMEA();
+            nmeaParser.MessageReceived += NmeaParser_MessageReceived;
+
+            String data = File.ReadAllText(@"c:\Vyvoj\SkolaFEI\MS\NMEA_Parser\NmeaParser\NmeaParser\Data\20090804-174057.nmea.txt");
+            nmeaParser.AddData(data);
+           
+        }
+
+        private void NmeaParser_MessageReceived(object sender, NMEAEventArgs e)
+        {
+           
         }
     }
 }
